6f675ea29a135aac78b000056dbf4550dfc0ba74
leobig99 <leobig99@gmail.com>
2017-04-17 20:57:17 +0200

    manca il tempo di attesa per visulaizzare la carta

--- Memory/src/memory/FXMLDocumentController.java
+++ Memory/src/memory/FXMLDocumentController.java
@@ -9,6 +9,10 @@ import Memo.memoryGioco;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.ResourceBundle;
+import java.util.Stack;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -66,118 +70,356 @@ public class FXMLDocumentController implements Initializable {
     private int conta;
     private int mossa1;
     private int mossa2;
-    private int mosse=0;
+    private int pos1;
+    private int pos2;
+    private Stack<ImageView> s;
+    private int mosse = 0;
 
     private boolean premuto[];
-            
+
     private boolean controllo() {
-        return mossa1==mossa2;
+        return mossa1 == mossa2;
     }
 
     @FXML
-    protected void mossauno(ActionEvent event) {
-        if(!premuto[0]){
-            premuto[0]=true;
+    protected void mossauno(ActionEvent event) throws InterruptedException {
+        if (!premuto[0]) {
+            premuto[0] = true;
             scopri(1);
-            if(conta==0){
-            mossa1=campo[0];
-            conta++;
-            mosse++;
-            nMosse.setText(""+mosse);
-        }else if(conta==1){
-            mossa2=campo[0];
-            conta++;
-            if(controllo()){
+            this.s.push(uno);
+            if (conta == 0) {
+                mossa1 = campo[0];
+                pos1 = 0;
+                conta++;
                 mosse++;
-                uno.setImage(empty);
-                setIm(m.getpos(mossa1),empty);
-                nMosse.setText(""+mosse);
-                conta=0;
-            }else{
+                nMosse.setText("" + mosse);
+            } else if (conta == 1) {
+                mossa2 = campo[0];
+                pos2 = 0;
+                conta++;
+                if (controllo()) {
+                    s.removeAllElements();
+                } else {
+                    while (!s.empty()) {
+                        s.pop().setImage(this.retro);
+                    }
+                    premuto[pos1] = false;
+                    premuto[pos2] = false;
+                }
                 mosse++;
-                uno.setImage(retro);
-                setIm(m.getpos(mossa1),retro);
-                premuto[0]=false;
-                nMosse.setText(""+mosse);
-                conta=0;
+                conta = 0;
+                nMosse.setText("" + mosse);
             }
         }
-        }
-        
-        
+
     }
 
     @FXML
-    protected void mossadue(ActionEvent event) {
-        if(!premuto[1]){
-            premuto[1]=true;
+    protected void mossadue(ActionEvent event) throws InterruptedException {
+        if (!premuto[1]) {
+            premuto[1] = true;
             scopri(2);
-        if(conta==0){
-            mossa1=campo[1];
-            conta++;
-            mosse++;
-            nMosse.setText(""+mosse);
-        }else if(conta==1){
-            mossa2=campo[1];
-            conta++;
-            if(controllo()){
+            this.s.push(due);
+            if (conta == 0) {
+                mossa1 = campo[1];
+                pos1 = 1;
+                conta++;
                 mosse++;
-                due.setImage(empty);
-                setIm(m.getpos(mossa1),empty);
-                nMosse.setText(""+mosse);
-                conta=0;
-            }else{
+                nMosse.setText("" + mosse);
+            } else if (conta == 1) {
+                mossa2 = campo[1];
+                pos2 = 1;
+                conta++;
+                if (controllo()) {
+                    s.removeAllElements();
+//                    setIm(m.getpos(mossa1), empty);
+                } else {
+//                    TimeUnit.SECONDS.sleep(1);
+
+                    while (!s.empty()) {
+                    }
+                    premuto[pos1] = false;
+                    premuto[pos2] = false;
+                }
                 mosse++;
-                due.setImage(retro);
-                setIm(m.getpos(mossa1),retro);
-                premuto[1]=false;
-                nMosse.setText(""+mosse);
-                conta=0;
+                conta = 0;
+                nMosse.setText("" + mosse);
             }
         }
+    }
+
+    @FXML
+    protected void mossatre(ActionEvent event) throws InterruptedException {
+        if (!premuto[2]) {
+            premuto[2] = true;
+            scopri(3);
+            this.s.push(tre);
+            if (conta == 0) {
+                mossa1 = campo[2];
+                pos1 = 2;
+                conta++;
+                mosse++;
+                nMosse.setText("" + mosse);
+            } else if (conta == 1) {
+                mossa2 = campo[2];
+                pos2 = 2;
+                conta++;
+                if (controllo()) {
+                    s.removeAllElements();
+                } else {
+//                    TimeUnit.SECONDS.sleep(1);
+
+                    while (!s.empty()) {
+                        s.pop().setImage(this.retro);
+                    }
+                    premuto[pos1] = false;
+                    premuto[pos2] = false;
+                }
+                mosse++;
+                conta = 0;
+                nMosse.setText("" + mosse);
+            }
         }
     }
 
     @FXML
-    protected void mossatre(ActionEvent event) {
-        scopri(3);
+    protected void mossaquatto(ActionEvent event) throws InterruptedException {
+        if (!premuto[3]) {
+            premuto[3] = true;
+            scopri(4);
+            this.s.push(quattro);
+            if (conta == 0) {
+                mossa1 = campo[3];
+                pos1 = 3;
+                conta++;
+                mosse++;
+                nMosse.setText("" + mosse);
+            } else if (conta == 1) {
+                mossa2 = campo[3];
+                pos2 = 3;
+                conta++;
+                if (controllo()) {
+                    s.removeAllElements();
+                } else {
+//                    TimeUnit.SECONDS.sleep(1);
+
+                    while (!s.empty()) {
+                        s.pop().setImage(this.retro);
+                    }
+                    premuto[pos1] = false;
+                    premuto[pos2] = false;
+                }
+                mosse++;
+                conta = 0;
+                nMosse.setText("" + mosse);
+            }
+        }
     }
 
     @FXML
-    protected void mossaquatto(ActionEvent event) {
-        scopri(4);
+    protected void mossacinque(ActionEvent event) throws InterruptedException {
+        if (!premuto[4]) {
+            premuto[4] = true;
+            scopri(5);
+            this.s.push(cinque);
+            if (conta == 0) {
+                mossa1 = campo[4];
+                pos1 = 4;
+                conta++;
+                mosse++;
+                nMosse.setText("" + mosse);
+            } else if (conta == 1) {
+                mossa2 = campo[4];
+                pos2 = 4;
+                conta++;
+                if (controllo()) {
+                    s.removeAllElements();
+                } else {
+//                    TimeUnit.SECONDS.sleep(1);
+
+                    while (!s.empty()) {
+                        s.pop().setImage(this.retro);
+                    }
+                    premuto[pos1] = false;
+                    premuto[pos2] = false;
+                }
+                mosse++;
+                conta = 0;
+                nMosse.setText("" + mosse);
+            }
+        }
     }
 
     @FXML
-    protected void mossacinque(ActionEvent event) {
-        scopri(5);
+    protected void mossasei(ActionEvent event) throws InterruptedException {
+        if (!premuto[5]) {
+            premuto[5] = true;
+            scopri(6);
+            this.s.push(sei);
+            if (conta == 0) {
+                mossa1 = campo[5];
+                pos1 = 5;
+                conta++;
+                mosse++;
+                nMosse.setText("" + mosse);
+            } else if (conta == 1) {
+                mossa2 = campo[5];
+                pos2 = 5;
+                conta++;
+                if (controllo()) {
+                    s.removeAllElements();
+                } else {
+//                    TimeUnit.SECONDS.sleep(1);
+
+                    while (!s.empty()) {
+                        s.pop().setImage(this.retro);
+                    }
+                    premuto[pos1] = false;
+                    premuto[pos2] = false;
+                }
+                mosse++;
+                conta = 0;
+                nMosse.setText("" + mosse);
+            }
+        }
     }
 
     @FXML
-    protected void mossasei(ActionEvent event) {
-        scopri(6);
+    protected void mossasette(ActionEvent event) throws InterruptedException {
+        if (!premuto[6]) {
+            premuto[6] = true;
+            scopri(7);
+            this.s.push(sette);
+            if (conta == 0) {
+                mossa1 = campo[6];
+                pos1 = 6;
+                conta++;
+                mosse++;
+                nMosse.setText("" + mosse);
+            } else if (conta == 1) {
+                mossa2 = campo[6];
+                pos2 = 6;
+                conta++;
+                if (controllo()) {
+                    s.removeAllElements();
+                } else {
+//                    TimeUnit.SECONDS.sleep(1);
+
+                    while (!s.empty()) {
+                        s.pop().setImage(this.retro);
+                    }
+                    premuto[pos1] = false;
+                    premuto[pos2] = false;
+                }
+                mosse++;
+                conta = 0;
+                nMosse.setText("" + mosse);
+            }
+        }
     }
 
     @FXML
-    protected void mossasette(ActionEvent event) {
-        scopri(7);
+    protected void mossaotto(ActionEvent event) throws InterruptedException {
+        if (!premuto[7]) {
+            premuto[7] = true;
+            scopri(8);
+            this.s.push(otto);
+            if (conta == 0) {
+                mossa1 = campo[7];
+                pos1 = 7;
+                conta++;
+                mosse++;
+                nMosse.setText("" + mosse);
+            } else if (conta == 1) {
+                mossa2 = campo[7];
+                pos2 = 7;
+                conta++;
+                if (controllo()) {
+                    s.removeAllElements();
+                } else {
+//                    TimeUnit.SECONDS.sleep(1);
+
+                    while (!s.empty()) {
+                        s.pop().setImage(this.retro);
+                    }
+                    premuto[pos1] = false;
+                    premuto[pos2] = false;
+                }
+                mosse++;
+                conta = 0;
+                nMosse.setText("" + mosse);
+            }
+        }
     }
 
     @FXML
-    protected void mossaotto(ActionEvent event) {
-        scopri(8);
+    protected void mossanove(ActionEvent event) throws InterruptedException {
+        if (!premuto[8]) {
+            premuto[8] = true;
+            scopri(9);
+            this.s.push(nove);
+            if (conta == 0) {
+                mossa1 = campo[8];
+                pos1 = 8;
+                conta++;
+                mosse++;
+                nMosse.setText("" + mosse);
+            } else if (conta == 1) {
+                mossa2 = campo[8];
+                pos2 = 8;
+                conta++;
+                if (controllo()) {
+                    s.removeAllElements();
+                } else {
+//                    TimeUnit.SECONDS.sleep(1);
+
+                    while (!s.empty()) {
+                        s.pop().setImage(this.retro);
+                    }
+                    premuto[pos1] = false;
+                    premuto[pos2] = false;
+                }
+                mosse++;
+                conta = 0;
+                nMosse.setText("" + mosse);
+            }
+        }
     }
 
     @FXML
-    protected void mossanove(ActionEvent event) {
-        scopri(9);
+    protected void mossadieci(ActionEvent event) throws InterruptedException {
+        if (!premuto[9]) {
+            premuto[9] = true;
+            scopri(10);
+            this.s.push(dieci);
+            if (conta == 0) {
+                mossa1 = campo[9];
+                pos1 = 9;
+                conta++;
+                mosse++;
+                nMosse.setText("" + mosse);
+            } else if (conta == 1) {
+                mossa2 = campo[9];
+                pos2 = 9;
+                conta++;
+                if (controllo()) {
+                    s.removeAllElements();
+                } else {
+//                    TimeUnit.SECONDS.sleep(1);
+
+                    while (!s.empty()) {
+                        s.pop().setImage(this.retro);
+                    }
+                    premuto[pos1] = false;
+                    premuto[pos2] = false;
+                }
+                mosse++;
+                conta = 0;
+                nMosse.setText("" + mosse);
+            }
+        }
     }
 
-    @FXML
-    protected void mossadieci(ActionEvent event) {
-        scopri(10);
-    }
-    
     @FXML
     protected void reset(ActionEvent event) {
         this.initialize(null, null);
@@ -257,10 +499,11 @@ public class FXMLDocumentController implements Initializable {
         nove.setImage(retro);
         dieci.setImage(retro);
         mossa1 = mossa2 = conta = 0;
-        nMosse.setText(""+0);
-        mosse=0;
-        premuto=new boolean[m.getCampo().length];
-        Arrays.fill(premuto,false);
+        nMosse.setText("" + 0);
+        mosse = 0;
+        this.s = new Stack();
+        premuto = new boolean[m.getCampo().length];
+        Arrays.fill(premuto, false);
     }
 
 }
